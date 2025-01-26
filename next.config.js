b76b0cbf146371d66ebdd545c8816438b42a6338
saket <saki007ster@gmail.com>
2025-01-26 10:31:02 -0500

    Updating for SSR.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,15 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  basePath = `/${repo}`;
-  assetPrefix = `/${repo}/`;
-}
-
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
-    unoptimized: true, // Required for GitHub Pages
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
   },
-  trailingSlash: true, // Ensures proper file structure for static hosting
 };
 
 module.exports = nextConfig;
